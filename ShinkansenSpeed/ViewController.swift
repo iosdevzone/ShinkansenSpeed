60d2443e20161135f9f222b7e95ae55c036f4955
iosdevzone <idz@iosdeveloperzone.com>
2016-05-05 02:45:01 -0700

    Template for ShinkansenSpeed Tutorial

---
+++ ShinkansenSpeed/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ShinkansenSpeed
+//
+//  Created by idz on 6/30/15.
+//  Copyright (c) 2015-2016 iOS Developer Zone. 
+//  License: MIT https://raw.githubusercontent.com/iosdevzone/ShinkansenSpeed/master/LICENSE
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
+
